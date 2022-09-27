58eda8c7e4b318812d68266aeca97cc6c36cfe3d
paige-clark <paigeclarkillustration@gmail.com>
2022-09-27 18:44:54 +0000

    Add test that checks to see if plant is added to cart from the homepage

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,23 @@
+describe('User can navigate from home to a product', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  /*
+  Write a new feature spec that will test that users can click the
+  'Add to Cart' button for a product on the home page and in doing
+  so their cart increases by one.
+  */
+  // first check to see if there are two products on the page
+  it('There are 2 products on the page', () => {
+    cy.get('.products article').should('have.length', 2);
+  });
+  // first check to see if there are two products on the page
+  it('Get the button for the first element', () => {
+    cy.get('.products article:first button')
+      .should('contain.text', 'Add')
+      .click({ force: true });
+    cy.get('nav')
+      .should('contain.text', 'My Cart (1)')
+    
+  });
+});
